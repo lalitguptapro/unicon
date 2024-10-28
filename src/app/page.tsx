860f873404a05fe7cc2ae43f99164eb3c337c028
Lalit Gupta <115988110+lalitguptapro@users.noreply.github.com>
2024-10-28 22:42:56 +0530

    uoda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-import Aboutus from '@/components/home/Aboutus'
-import Banner from '@/components/home/Banner'
-import Blog from '@/components/home/Blog'
-import Capabilities from '@/components/home/Capabilities'
-import Clients from '@/components/home/Clients'
-import Homecta from '@/components/home/Homecta'
-import Services from '@/components/home/Services'
-import Whyus from '@/components/home/Whyus'
-import Head from 'next/head'
-import React from 'react'
+import Aboutus from '@/components/home/Aboutus';
+import Banner from '@/components/home/Banner';
+import Blog from '@/components/home/Blog';
+import Capabilities from '@/components/home/Capabilities';
+import Clients from '@/components/home/Clients';
+import Homecta from '@/components/home/Homecta';
+import Services from '@/components/home/Services';
+import Whyus from '@/components/home/Whyus';
+import Head from 'next/head';
+import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-    <Head>
-      <title>Your Company Name | Innovative Solutions and Services</title>
-      <meta name="description" content="Discover our innovative solutions, expert services, and client success stories. Learn how we can help your business thrive." />
-      <meta name="google-site-verification" content="MugTmYFPPG22wGWnDjAARRtVPPtzjQEfvBIx8XYUMuo" />
-    </Head>
-    <Banner />
-    <Clients />
-    <Aboutus />
-    <Services />
-    {/* <Portfolio /> */}
-    <Capabilities />
-    <Whyus />
-    <Blog />
-    <Homecta />
-  </div>
-  )
+      <Head>
+        <title>Your Company Name | Innovative Solutions and Services</title>
+        <meta name="description" content="Discover our innovative solutions, expert services, and client success stories. Learn how we can help your business thrive." />
+        <meta name="google-site-verification" content="MugTmYFPPG22wGWnDjAARRtVPPtzjQEfvBIx8XYUMuo" />
+      </Head>
+      <Banner />
+      <Clients />
+      <Aboutus />
+      <Services />
+      {/* <Portfolio /> */}
+      <Capabilities />
+      <Whyus />
+      <Blog />
+      <Homecta />
+    </div>
+  );
 }
 
-export default page
+export default Page;
